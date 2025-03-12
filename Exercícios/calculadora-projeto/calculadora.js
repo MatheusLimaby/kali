062b397b3c6b3f254681d51481154698008293fe
MatheusLimaby <Matheus.l.Rodrigues@iesb.edu.br>
2025-03-12 20:54:16 -0300

    Atividade 3

---
+++ Exercícios/calculadora-projeto/calculadora.js
@@ -0,0 +1,15 @@
+ export const soma = function(a,b) {
+    return a + b
+ }
+
+ export const subtracao = function(a,b) {
+    return a - b
+ }
+
+ export const multiplicacao = function(a,b) {
+    return a * b
+ }
+
+ export const divisao = function(a,b) {
+    return a / b
+ }
