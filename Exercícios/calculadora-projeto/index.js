675509ecdd9b9b1e45c6aaa4688a8d85a110b25f
MatheusLimaby <Matheus.l.Rodrigues@iesb.edu.br>
2025-03-12 21:03:30 -0300

    Atividade 3

--- Exercícios/calculadora-projeto/index.js
+++ Exercícios/calculadora-projeto/index.js
@@ -1,9 +1,14 @@
+//Atividade 1
+
 import {soma, subtracao, multiplicacao, divisao} from './calculadora.js'
 
 console.log("Soma:", soma(10, 5));
 console.log("Subtração:", subtracao(10, 5));
 console.log("Multiplicação:", multiplicacao(10, 5));
 console.log("Divisão:", divisao(10, 5));
+
+
+//Atividade 2
 
 import moment from 'moment';
 function calcularIdade(anoNascimento) {
